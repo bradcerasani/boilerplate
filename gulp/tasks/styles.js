128d8f2569ebc7405ff6bd8076fb2849cb1c1f3b
Brad Cerasani <bradcerasani@gmail.com>
2014-05-11 12:32:58 -0500

    added prefixer and csso

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,11 +1,15 @@
-var sass         = require('gulp-sass');
 var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var prefix       = require('gulp-autoprefixer');
+var csso         = require('gulp-csso');
 var notify       = require('gulp-notify');
 var handleErrors = require('../util/handleErrors');
 
 gulp.task('styles', function() {
 	return gulp.src('./src/sass/*.scss')
 		.pipe(sass())
+    .pipe(prefix("last 2 version"))
+    .pipe(csso())
 		.on('error', handleErrors)
     .pipe(gulp.dest('./build/stylesheets'));
 });
