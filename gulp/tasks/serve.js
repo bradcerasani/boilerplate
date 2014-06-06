9e45c94f22d10cb8e81bd84c9219dde2448b5a97
Brad Cerasani <bradcerasani@gmail.com>
2014-06-06 18:19:53 -0500

    replaced livereload with browser-sync

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,12 +1,10 @@
 var gulp = require("gulp");
-var connect = require('connect');
-var http = require('http');
-var config = require('../config');
+var browserSync = require('browser-sync');
 
-gulp.task('serve', function(){
-  var app = connect()
-    .use(connect.logger('dev'))
-    .use(connect.static(config.root));
-
-  http.createServer(app).listen(config.port);
+gulp.task('serve', function() {
+  browserSync.init(null, {
+    server: {
+      baseDir: "./"
+    }
+  });
 });
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
-var csso = require('gulp-csso');
 var rename = require('gulp-rename');
 var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync');
 var error = require('../lib/error');
 
 gulp.task('styles', function() {
@@ -12,8 +12,5 @@ gulp.task('styles', function() {
     .on('error', error)
     .pipe(prefix("last 2 version"))
     .pipe(gulp.dest('./build/assets/stylesheets'))
-    .pipe(livereload())
-    .pipe(csso())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./build/assets/stylesheets'));
+    .pipe(browserSync.reload({stream:true, notify: true}));
 });
