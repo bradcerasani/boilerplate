eff49e159ff4d303ac11ff3b2cd53c290cbb5a0b
Brad Cerasani <bradcerasani@gmail.com>
2014-06-08 17:21:51 -0500

    watch task tweaks

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,6 @@ var livereload = require('gulp-livereload');
 
 gulp.task('watch', function() {
   var server = livereload();
-
   var reload = function(file) {
     server.changed(file.path);
   };
@@ -12,5 +11,4 @@ gulp.task('watch', function() {
   gulp.watch('src/assets/javascripts/**', ['scripts']);
   gulp.watch('src/assets/stylesheets/**', ['styles']);
   gulp.watch('src/assets/images/**', ['images']);
-  // gulp.watch(['build/**']).on('change', reload);
 });
