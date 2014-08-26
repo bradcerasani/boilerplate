dbe2780b68edb45d3abc3679315c94d27b519268
Brad Cerasani <bradcerasani@gmail.com>
2014-08-25 21:12:40 -0500

    added fileinclude example

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 gulp.task('html', function() {
-  return gulp.src('./src/*.html')
+  return gulp.src('./src/*.html', '!src/partials/*.html')
     .pipe(gulp.dest('./build'))
     .pipe(browserSync.reload({stream:true}));
 });
