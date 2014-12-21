1506edca34a82928d93957d57ab266f93ef5bb29
Brad Cerasani <bradcerasani@gmail.com>
2014-12-21 10:10:08 -0600

    added target browser array for autoprefixer, increased sass precision

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,17 +2,30 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
-var rename = require('gulp-rename');
+var changed = require('gulp-changed');
 var browserSync = require('browser-sync');
 var error = require('../lib/error');
 
+var TARGET_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
+
 gulp.task('styles', function() {
-  return gulp.src('./src/assets/stylesheets/*.scss')
+  return gulp.src('./src/assets/stylesheets/main.scss')
+    .pipe(changed('styles', {extension: '.scss'}))
     .pipe(sourcemaps.init())
-    .pipe(sass())      
+    .pipe(sass({precision: 10}))      
     .on('error', error)
     .pipe(sourcemaps.write())
-    .pipe(prefix("last 2 version"))
+    .pipe(prefix({browsers: TARGET_BROWSERS}))
     .pipe(gulp.dest('./build/assets/stylesheets'))
     .pipe(browserSync.reload({stream:true}));
 });
