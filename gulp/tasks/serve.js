dc30140e1002455ce29de56a583722e19aeac39d
Brad Cerasani <bradcerasani@gmail.com>
2014-12-21 00:24:35 -0600

    serve task cleanup

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,15 +1,13 @@
 var gulp = require("gulp");
 var browserSync = require('browser-sync');
 
-var options = {
-  xip: true,
-  notify: false,
-  logPrefix: "BrowserSync",
-  server: {
-    baseDir: "./build"
-  }
-}
-
 gulp.task('serve', function() {
-  browserSync.init(['build/**'], options);
+  browserSync({
+    notify: false,
+    open: false,
+    logPrefix: "BrowserSync",
+    server: {
+      baseDir: "./build"
+    }
+  });
 });
