807bbe5943f35b9cdd571676955e7e2d29df99bd
Brad Cerasani <bradcerasani@gmail.com>
2014-12-20 23:25:28 -0600

    added sass sourcemaps

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var prefix = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync');
@@ -7,8 +8,10 @@ var error = require('../lib/error');
 
 gulp.task('styles', function() {
   return gulp.src('./src/assets/stylesheets/*.scss')
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass())      
     .on('error', error)
+    .pipe(sourcemaps.write())
     .pipe(prefix("last 2 version"))
     .pipe(gulp.dest('./build/assets/stylesheets'))
     .pipe(browserSync.reload({stream:true, notify: false}));
